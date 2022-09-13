031f99a12fc9fa29fc1e026c69262fd085cf40d5
Nithin Kumar M <nithinbhargav007@gmail.com>
2022-09-13 06:12:23 +0530

    Updated the queue code

--- queue_delete.sv
+++ queue_delete.sv
@@ -20,24 +20,35 @@ module top;
       w.to_remove = $urandom_range(0,1);
       q.push_back(w);
       $display("Widget id : %0d , to_remove : %0b", q[$].id, q[$].to_remove); 
-  
     end
+    
+    
     for(int i=0;i<q.size;i++)
         if(q[i].to_remove == 1'b1)
           begin
-            $display("q[i].id = %0d , q[i].to_remove = %0b", q[i].id, q[i].to_remove);
             q.delete(i);
+            i--;
           end
       //display the queue after deletion
           
-          foreach(q[i])
+      foreach(q[i])
             $display("Display : q[i].id = %0d , q[i].to_remove = %0b", q[i].id, q[i].to_remove);
     
+
+   for(int j=0;j<q.size;j++)
+      begin
+        if(q[j].to_remove == 1'b1)
+        $display("There is an entry in the queue which is still not removed");
+        break;
+      end
+         $display("All the required entries in the queue are removed");
+     
   end
   
   
   
 endmodule
+  
   
   
   
