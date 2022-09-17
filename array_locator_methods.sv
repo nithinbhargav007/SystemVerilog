ee0c0317c4ba456bf73840cb4eb70d69f0903dde
Nithin Kumar M <nithinbhargav007@gmail.com>
2022-09-17 07:22:59 +0530

    array locator methods updated

--- array_locator_methods.sv
+++ array_locator_methods.sv
@@ -21,5 +21,21 @@ module tb;
 
     res = array.find_last_index(x) with (x < 3);
     $display ("find_last_index : %p", res);
+
+        res = array.min();
+    $display ("min          : %p", res);
+
+    res = array.max();
+    $display ("max          : %p", res);
+
+    res = array.unique();
+    $display ("unique       : %p", res);
+
+    res = array.unique(x) with (x < 3);
+    $display ("unique       : %p", res);
+
+    res = array.unique_index;
+    $display ("unique_index : %p", res);
+    
   end
 endmodule
